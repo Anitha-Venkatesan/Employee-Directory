12684689c40b0af77197a7e45d688ef629a719fb
Anitha Venkatesan <anithamca68@gmail.com>
2020-08-10 10:44:15 -0500

    Added sort functionality for name field

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import React  from "react";
 import Employee from "./components/employee";
 import "./App.css";
 import data from "./data/employee.json";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
-export default function App() {
+class App extends React.Component {
+  state = {
+    data
+  }
 
+  sortByName= () => {
+    this.setState({
+      data: data.sort( (a , b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      })
+    });
+  }
+  
+render() {
   return (
     <div>
-      <p>Employee Directory Using React</p>
-      <Employee data={data}/>
+      <Navbar />
+      <Search/>
+      <Employee data={this.state.data}
+      sortByName= {this.sortByName}
+      />
     </div>
-  )
-
+  );
+  }
 }
+export default App;
+
+
+
+
 
   
