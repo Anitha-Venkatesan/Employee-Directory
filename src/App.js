0809cc40ba19ec301ea4a6cdd955184c6d42c21d
Anitha Venkatesan <anithamca68@gmail.com>
2020-08-10 01:20:00 -0500

    using Employee component in return function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import Employee from "./components/employee";
+import "./App.css";
+import data from "./data/employee.json";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Employee Directory Using React</p>
+      <Employee data={data}/>
     </div>
-  );
+  )
+
 }
 
-export default App;
+  
