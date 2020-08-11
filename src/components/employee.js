10f4c93002b5376e239721de161a31b880a8f446
Anitha Venkatesan <anithamca68@gmail.com>
2020-08-11 16:08:15 -0500

    Added styles for the tables using react bootstrap

--- src/components/employee.js
+++ src/components/employee.js
@@ -1,40 +1,48 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 
 export default function Employee(props) {
     return (
-        <table>
-            <thead>
-                <tr>
-                 <th onClick = {() => props.sortByName("name")}>Name
+        <Container className = "mt-5">
+            <Col>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th onClick={() => props.sortByName("name")}>Name
                  </th>
-                 <th>Role</th>
-                 <th>Email</th>
-                 <th>Mobile</th>
-                </tr>
-            </thead>
-            <tbody>
-                {//map to loop through each item in the array
-                    props.data.map(row  => (
-                        <tr key = {row.id}>
-                            <td>
-                                {row.name}
-                            </td>
-                            <td>
-                                {row.role}
-                            </td>
-                            <td>
-                                {row.email}
-                            </td>
-                            <td>
-                                {row.mobile}
-                            </td>    
+                            <th>Role</th>
+                            <th>Email</th>
+                            <th>Mobile</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        {//map to loop through each item in the array
+                            props.data.map(row => (
+                                <tr key={row.id}>
+                                    <td>
+                                        {row.name}
+                                    </td>
+                                    <td>
+                                        {row.role}
+                                    </td>
+                                    <td>
+                                        {row.email}
+                                    </td>
+                                    <td>
+                                        {row.mobile}
+                                    </td>
+                                </tr>
 
-                    ))
-                }
+                            ))
+                        }
 
-            </tbody>
-        </table>
+                    </tbody>
+                </Table>
+            </Col>
+
+        </Container>
     )
 
 }
